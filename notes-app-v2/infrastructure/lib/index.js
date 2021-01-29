b65416c9cf5d88c2a11564df61a65e167daf250d
Brandon <brandon.dlamini95@gmail.com>
2021-01-29 10:08:11 +0200

    added Cognito User pool and identity Pool stack

--- notes-app-v2/infrastructure/lib/index.js
+++ notes-app-v2/infrastructure/lib/index.js
@@ -1,8 +1,11 @@
 import DynamoDBStack from "./DynamoDBStack";
-import S3Stack from "./S3Stack"
+import S3Stack from "./S3Stack";
+import CognitoStack from "./CognitoStack";
 // Add stacks
 export default function main(app) {
   new DynamoDBStack(app, "dynamodb");
 
   new S3Stack(app, "s3");
+
+  new CognitoStack(app, "coginito");
 }
