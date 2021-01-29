b7c77837ff0dcab94bf461ac115c0cbd2a4c2596
Brandon <brandon.dlamini95@gmail.com>
2021-01-29 12:33:48 +0200

    FIXED S3

--- notes-app-v2/infrastructure/lib/index.js
+++ notes-app-v2/infrastructure/lib/index.js
@@ -1,13 +1,12 @@
-import DynamoDBStack from "./DynamoDBStack";
 import S3Stack from "./S3Stack";
 import CognitoStack from "./CognitoStack";
+import DynamoDBStack from "./DynamoDBStack";
+
 // Add stacks
 export default function main(app) {
   new DynamoDBStack(app, "dynamodb");
 
-  new S3Stack(app, "s3");
+  const s3 = new S3Stack(app, "s3");
 
-  nnew CognitoStack(app, "cognito", { bucketArn: s3.bucket.bucketArn });
-
-
+  new CognitoStack(app, "cognito", { bucketArn: s3.bucket.bucketArn });
 }
