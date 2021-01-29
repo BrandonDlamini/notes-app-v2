736f70de6e455bae8b9d4e672d347f09094f5425
Brandon <brandon.dlamini95@gmail.com>
2021-01-29 09:44:41 +0200

    add S3 and Cognitio stack

--- notes-app-v2/infrastructure/lib/index.js
+++ notes-app-v2/infrastructure/lib/index.js
@@ -1,6 +1,8 @@
 import DynamoDBStack from "./DynamoDBStack";
-
+import S3Stack from "./S3Stack"
 // Add stacks
 export default function main(app) {
   new DynamoDBStack(app, "dynamodb");
+
+  new S3Stack(app, "s3");
 }
